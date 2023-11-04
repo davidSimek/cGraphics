7e24e8fdac380d930df870c470d3333ff1988e00
davidSimek <david.simek@infis.cz>
2023-11-04 20:00:41 +0100

    nono erooor

--- main.c
+++ main.c
@@ -6,8 +6,10 @@
 
 #define THREAD_COUNT 10 
 
+// this is extremely general structure
+// you simply just pass it arguments in struct which you have to cast to void
+// then you pass function that follows void (*execute)(void*) signature and it will pass it that args
 typedef struct {
-    int line;
     void* arg;
     void (*execute)(void*);
 } ThreadData;
@@ -26,10 +28,19 @@ typedef struct {
 } ImgBufferConfig;
 
 void* run(void* arg);
-void spawnThreads(ThreadData* data, pthread_t* thread);
+void spawnThreads(const ThreadData* data, pthread_t* thread, const int threadCount);
 int mergeThreads(ThreadData* data, pthread_t* thread);
 unsigned char* createBuffer(ImgBufferConfig* bufferConfig);
 int digitsCount(int num);
+
+// not part of library, user defined:
+typedef struct {
+    int line;
+} PerLineData;
+void perLine(void* s) {
+    PerLineData* data = (PerLineData*)s;
+    printf("currently on line %d\n", data->line);
+}
 
 
 int main(int argc, char *argv[])
@@ -37,17 +48,27 @@ int main(int argc, char *argv[])
     ImgBufferConfig bufferConfig = {100, 100, 0, 0}; 
     unsigned char* buffer = createBuffer(&bufferConfig);
 
+    // make array of thread data for each line
+    ThreadData* data = (ThreadData*)malloc(sizeof(pthread_t) * bufferConfig.height);
+    PerLineData* perLineData = (PerLineData*)malloc(sizeof(PerLineData) * bufferConfig.height);
     for (int i = 0; i < bufferConfig.height; i++) {
-        for (int j = 0; j < bufferConfig.width; j++) {
-            int index = 3 * (i * bufferConfig.height + j) + bufferConfig.headerSize;
-            buffer[index++] = 255; // R
-            buffer[index++] = 128; // G
-            buffer[index++] = 64;  // B
-        }   
+        perLineData[i].line = i;
+        data[i].arg = &perLineData[i];
+        data[i].execute = perLine;
     }
+
+    // make array of threads for each line
+    pthread_t* threads = (pthread_t*)malloc(sizeof(pthread_t) * bufferConfig.height);
+
+
+    spawnThreads(data, threads, bufferConfig.height);
+    mergeThreads(data, threads);
 
     printf("%s", buffer);
     free(buffer);
+    free(data);
+    free(threads);
+    free(perLineData);
     return 0;
 }
 
@@ -99,18 +120,28 @@ unsigned char* createBuffer(ImgBufferConfig* bufferConfig) {
     buffer[7 + widhtChars + heightChars] = '5';
     buffer[8 + widhtChars + heightChars] = '\n';
     
+    // fill it with white initialy | should be handled differently in future
+    for (int i = 0; i < bufferConfig->height; i++) {
+        for (int j = 0; j < bufferConfig->width; j++) {
+            int index = 3 * (i * bufferConfig->height + j) + bufferConfig->headerSize;
+            buffer[index++] = 255; // R
+            buffer[index++] = 255; // G
+            buffer[index++] = 255;  // B
+        }   
+    }
+    
     return buffer;
 }
 
 void* run(void* arg) {
-    ThreadData* data = (ThreadData*)arg;
-    data->execute(data->arg);
-    return 0;
+     ThreadData* data = (ThreadData*)arg;
+     data->execute(data->arg);
+     return 0;
 }
 
-void spawnThreads(ThreadData* data, pthread_t* thread) {
-    for (int i = 0; i < THREAD_COUNT; i++) {
-        if (pthread_create(&thread[i], NULL, run, (void*)data)) {
+void spawnThreads(const ThreadData* data, pthread_t* thread, const int threadCount) {
+    for (int i = 0; i < threadCount; i++) {
+        if (pthread_create(&thread[i], NULL, run, (void*)&data[i])) {
             fprintf(stderr, "lol, you will not make thread");
         }
     }
@@ -125,4 +156,3 @@ int mergeThreads(ThreadData* data, pthread_t* thread) {
     }
     return 0;
 }
-
